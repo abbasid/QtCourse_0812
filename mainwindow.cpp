a03f78626be6181be350d659ab38e6d269e71da1
Abbasid_ho <abbasid81725@gmail.com>
2015-08-12 11:02:47 +0800

    add file dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,17 +6,14 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QString str = "HEllo";
-    qDebug() << str;
-    str.prepend("hahaha");
-    qDebug() << str;
-    str.append("yaya");
-    qDebug() << str;
-    QString str2 = "momo";
-    QString str3 = str + str2;
-    qDebug() << str3;
-    str3.clear();
-    qDebug() << str3;
+    QString fileName;
+
+//    fileName = QFileDialog::getOpenFileName(this,
+//        tr("Open Image"), "/home/jana", tr("Image Files (*.png *.jpg *.bmp)"));
+
+    QString FileName;
+    QFileDialog fd;
+    FileName = fd.getOpenFileName();
 
 }
 
